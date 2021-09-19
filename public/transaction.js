b6298d93fdcabec36aaf65d30fac5e4e87b12049
Sianna Boschetti <77031557+sboschetti@users.noreply.github.com>
2021-09-19 14:38:33 -0400

    initial commit

---
+++ public/transaction.js
@@ -0,0 +1,14 @@
+
+
+function transaction() {
+    return (
+        <>
+            
+            <a className="btn btn-light" href="/#/deposit" onClick={Deposit}>Deposit</a>
+            <a className="btn btn-light" href="/#/withdraw" onClick={Withdraw}>Withdraw</a>
+            
+            
+        </>
+    );
+}
+
