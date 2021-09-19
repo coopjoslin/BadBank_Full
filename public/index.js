c790a909dda85185a0dbead59bdda9b7d4457493
Sianna Boschetti <77031557+sboschetti@users.noreply.github.com>
2021-09-19 15:18:52 -0400

    added auth

--- public/index.js
+++ public/index.js
@@ -7,7 +7,7 @@ function Spa() {
           <div className="container" style={{padding: "20px"}}>
             <Route path="/" exact component={Home} />
             <Route path="/CreateAccount/" component={CreateAccount} />
-            <Route path="/login/" component={Login} />
+            <Route path="/login/" component={login} />
             <Route path="/deposit/" component={Deposit} />
             <Route path="/withdraw/" component={Withdraw} />
             <Route path="/transaction/" component={transaction} />
--- public/login.js
+++ public/login.js
@@ -1,68 +1,113 @@
-function Login(){
-  const [show, setShow]     = React.useState(true);
-  const [status, setStatus] = React.useState('');    
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  return (
-    <Card
-      bgcolor="secondary"
-      header="Login"
-      status={status}
-      body={show ? 
-        <LoginForm setShow={setShow} setStatus={setStatus}/> :
-        <LoginMsg setShow={setShow} setStatus={setStatus}/>}
-    />
-  ) 
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCzLdWJEq0vOWSjfVA3C5OQxX--HO3kJmI",
+  authDomain: "badbank-bb18b.firebaseapp.com",
+  projectId: "badbank-bb18b",
+  storageBucket: "badbank-bb18b.appspot.com",
+  messagingSenderId: "745789819925",
+  appId: "1:745789819925:web:8ae5823019b3ac9692d21c"
+};
 
-function LoginMsg(props){
-  return(<>
-    <h5>Success</h5>
-    <button type="submit" 
-      className="btn btn-light" 
-      onClick={() => props.setShow(true)}>
-        Authenticate again
-    </button>
-  </>);
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export function login() {
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-function LoginForm(props){
-  const [email, setEmail]       = React.useState('');
-  const [password, setPassword] = React.useState('');
+  // get elements
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const login = document.getElementById("login");
+  const signup = document.getElementById("signup");
+  const logout = document.getElementById("logout");
+  const loggedInStatus = document.getElementById("loggedInStatus");
+  const googlelogin = document.getElementById("googlelogin");
 
-  function handle(){
-    fetch(`/account/login/${email}/${password}`)
-    .then(response => response.text())
-    .then(text => {
-        try {
-            const data = JSON.parse(text);
-            props.setStatus('');
-            props.setShow(false);
-            console.log('JSON:', data);
-        } catch(err) {
-            props.setStatus(text)
-            console.log('err:', text);
-        }
-    });
-  }
+  // login
+  login.addEventListener("click", (e) => {
+    const auth = firebase.auth();
+    const promise = auth.signInWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.catch((e) => console.log(e.message));
+  });
 
+  // signup
+  signup.addEventListener("click", (e) => {
+    // TODO: check for real email
+    const auth = firebase.auth();
+    const promise = auth.createUserWithEmailAndPassword(
+      email.value,
+      password.value
+    );
+    promise.catch((e) => console.log(e.message));
+  });
 
-  return (<>
+  //Google Login
+  googlelogin.addEventListener("click", (e) => {
+    console.log("google clicked");
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function (result) {
+        loggedInStatus.innerText = `You are logged in using the following email: ${result.user.email}`;
+        login.style.display = "none";
+        signup.style.display = "none";
+        email.style.display = "none";
+        password.style.display = "none";
+        googlelogin.style.display = "none";
+        logout.style.display = "none";
+      })
+      .catch(function (error) {
+        console.log(error.code);
+        console.log(error.message);
+      });
+  });
 
-    Email<br/>
-    <input type="input" 
-      className="form-control" 
-      placeholder="Enter email" 
-      value={email} 
-      onChange={e => setEmail(e.currentTarget.value)}/><br/>
+  // logout
+  logout.addEventListener("click", (e) => {
+    firebase.auth().signOut();
+  });
 
-    Password<br/>
-    <input type="password" 
-      className="form-control" 
-      placeholder="Enter password" 
-      value={password} 
-      onChange={e => setPassword(e.currentTarget.value)}/><br/>
+  // login state
+  firebase.auth().onAuthStateChanged((firebaseUser) => {
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      loggedInStatus.innerText = `You are logged in using the following email: ${result.user.email}`;
+      logout.style.display = "inline";
+      login.style.display = "none";
+      signup.style.display = "none";
+      email.style.display = "none";
+      password.style.display = "none";
+      googlelogin.style.display = "none";
+    } else {
+      console.log("User is not logged in");
+      loggedInStatus.innerText = "You are not yet logged in";
+      login.style.display = "inline";
+      signup.style.display = "inline";
+      email.style.display = "inline";
+      googlelogin.style.display = "inline";
+      password.style.display = "inline";
+      logout.style.display = "none";
+    }
+  });
 
-    <button type="submit" className="btn btn-light" onClick={handle}>Login</button>
-   
-  </>);
+return (
+  <>
+      <h1 id="loggedInStatus">You are not yet logged in</h1>
+      <input id="email" type="email" placeholder="Email"/><br/>
+      <input id="password" type="password" placeholder="Password"/><br/><br/>
+      <button id="login">Login</button>
+      <button id="signup">SignUp</button>
+      <button id="logout" style="display:none;">Logout</button>
+      <button id="googlelogin">Google Login</button>
+  </>
+);
 }
